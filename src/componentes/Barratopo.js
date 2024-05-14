efc71449fa7a8d260c5e16453556fec84871e913
rmtrescato <rmenezestrescato@gmail.com>
2024-05-14 10:12:01 -0300

    i

---
+++ src/componentes/Barratopo.js
@@ -0,0 +1,21 @@
+import styles from './Barratopo.model.css'
+
+
+
+function Barratopo(){
+  return (
+      <div>
+        <h1 className={styles.topo}>Teste de Identacao</h1>
+          <p id='navbar'>
+            Gmail
+            Imagens
+            </p> 
+             
+          
+
+      </div>
+  ) 
+
+}
+
+export default Barratopo
